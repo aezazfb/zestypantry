9d9fb21271d5b7d809e5a64698e1872da520b15d
az <aezazfb@hotmail.com>
2023-06-26 09:50:13 +0500

    Commit with Add Product Settings

--- lib/PersonalWidgets/AddProductWidget.dart
+++ lib/PersonalWidgets/AddProductWidget.dart
@@ -14,142 +14,165 @@ class AddProductWidget extends StatelessWidget {
   TextEditingController nameCtrl = TextEditingController();
   TextEditingController mobileCtrl  = TextEditingController();
   TextEditingController addressCtrl = TextEditingController();
+  TextEditingController descrCtrl = TextEditingController();
 
   final CollectionReference _red =
   FirebaseFirestore.instance.collection("product");
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container(
-          alignment: Alignment.centerLeft,
-          margin: EdgeInsets.all(8),
-          child: InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Icon(Icons.arrow_back_ios_new,
-              size: 28,),
-
-          ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.only(left: 15, top: 20),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Order Details",
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Color.fromARGB(162, 0, 0, 0),
-                    fontWeight: FontWeight.bold
-                ),
-
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 15, top: 15),
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              width: 370,
-              child: TextFormField(
-                controller: nameCtrl,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Name",
-                    hintStyle: TextStyle(
-                        fontSize: 20
-                    )
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 15, top: 15),
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              width: 370,
-              child: TextFormField(
-                controller: mobileCtrl,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Mobile",
-                    hintStyle: TextStyle(
-                        fontSize: 20
-                    )
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 15, top: 15),
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              width: 370,
-              child: TextFormField(
-                controller: addressCtrl,
-                keyboardType: TextInputType.datetime,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Address",
-                    hintStyle: TextStyle(
-                        fontSize: 20
-                    )
-                ),
-              ),
-            ),
-            SizedBox(height: 50,),
-            InkWell(
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
               onTap: () {
-
-                // DatabaseService(uid: 312.toString()).updateProduct(nameCtrl.text, int.parse(mobileCtrl.text.isNotEmpty == true ? mobileCtrl.text : "0"), addressCtrl.text).then((value){
-                //   Fluttertoast.showToast(msg: "Order Has Been Placed!");
-
-                // });
-
-                // _red.add({
-                //   'name': nameCtrl.text,
-                //   'expiry': addressCtrl.text,
-                //   'quantity':int.parse(mobileCtrl.text.isNotEmpty == true ? mobileCtrl.text : "0")
-                // }).whenComplete(() => Fluttertoast.showToast(msg: "New Item added!"));
-
-                addProduct(Product(
-                  name: nameCtrl.text,
-                  price: int.parse(addressCtrl.text),
-                  unit: mobileCtrl.text.isNotEmpty == true ? mobileCtrl.text : "gram"
-                )).whenComplete(() => Fluttertoast.showToast(msg: "New Product added!"));
-
+                Navigator.pop(context);
               },
-              child: Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                margin: EdgeInsets.symmetric(horizontal: 20),
-                child: Text("Order Now",
+              child: const Icon(Icons.arrow_back_ios_new,
+                size: 28,),
+
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 20),
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  "Product Details",
                   style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
+                      fontSize: 25,
+                      color: Color.fromARGB(162, 0, 0, 0),
                       fontWeight: FontWeight.bold
-                  ),),
+                  ),
+
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
                 decoration: BoxDecoration(
-                    color: Color(0xFFFFB608),
+                    color: Colors.white,
                     borderRadius: BorderRadius.circular(10)
                 ),
+                width: 370,
+                child: TextFormField(
+                  controller: nameCtrl,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Name",
+                      hintStyle: TextStyle(
+                          fontSize: 20
+                      )
+                  ),
+                ),
               ),
-            )
-          ],
-        )
-      ],
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                width: 370,
+                child: TextFormField(
+                  controller: descrCtrl,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "descr",
+                      hintStyle: TextStyle(
+                          fontSize: 20
+                      )
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                width: 370,
+                child: TextFormField(
+                  controller: mobileCtrl,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Unit",
+                      hintStyle: TextStyle(
+                          fontSize: 20
+                      )
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                width: 370,
+                child: TextFormField(
+                  controller: addressCtrl,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Price",
+                      hintStyle: TextStyle(
+                          fontSize: 20
+                      )
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50,),
+              InkWell(
+                onTap: () {
+
+                  // DatabaseService(uid: 312.toString()).updateProduct(nameCtrl.text, int.parse(mobileCtrl.text.isNotEmpty == true ? mobileCtrl.text : "0"), addressCtrl.text).then((value){
+                  //   Fluttertoast.showToast(msg: "Order Has Been Placed!");
+
+                  // });
+
+                  // _red.add({
+                  //   'name': nameCtrl.text,
+                  //   'expiry': addressCtrl.text,
+                  //   'quantity':int.parse(mobileCtrl.text.isNotEmpty == true ? mobileCtrl.text : "0")
+                  // }).whenComplete(() => Fluttertoast.showToast(msg: "New Item added!"));
+
+                  addProduct(Product(
+                    name: nameCtrl.text,
+                    price: int.parse(addressCtrl.text),
+                    unit: mobileCtrl.text.isNotEmpty == true ? mobileCtrl.text : "gram",
+                    description: descrCtrl.text
+                  )).whenComplete(() => Fluttertoast.showToast(msg: "New Product added!"));
+
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                      color: const Color(0xFFFFB608),
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: const Text("Order Now",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold
+                    ),),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
